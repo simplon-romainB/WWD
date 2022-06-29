2afe5731ea6070b76dafc7cec750f784cb2a54e0
simplon-romainB <neriaa@hotmail.fr>
2022-06-29 16:43:41 +0200

    generique

--- index.php
+++ index.php
@@ -4,11 +4,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/styles/main.css">
+    <link rel="stylesheet" href="/assets/styles/main.css">
     <link rel="stylesheet" href="/css/footerstyle.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Stoke:wght@300;400&display=swap" rel="stylesheet"> 
+    <script src="/assets/scripts/footer.js"></script>
     <title>W.W.D</title>
 </head>
 <body>
+    
     <?php include "footer.html" ?>
 </body>
 </html>
