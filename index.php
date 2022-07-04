4a84ff961ba58cf88ea6b6a50d6464b8859033b7
Axinellaz <axinellazehi@gmail.com>
2022-07-04 16:06:33 +0200

    master

--- index.php
+++ index.php
@@ -6,9 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/styles/main.css">
     <link rel="stylesheet" href="/css/footerstyle.css">
+    <link rel="stylesheet" href="/css/headerStyle.css">
+    <link href="https://fonts.googleapis.com/css2?family=Bodoni+Moda:ital,opsz,wght@0,6..96,400;1,6..96,400;1,6..96,500;1,6..96,600;1,6..96,700;1,6..96,800;1,6..96,900&family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <title>W.W.D</title>
 </head>
 <body>
+    <?php include "header.html" ?>
     <?php include "footer.html" ?>
 </body>
 </html>
